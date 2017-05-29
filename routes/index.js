eb6d9bb75aa45914a5be8c95e8f58d027a6acb24
adaptedbee <adaptedbee@yandex.ru>
2017-05-29 18:22:59 +0300

    31. JSON endpoints and creating MongoDB Indexes

--- routes/index.js
+++ routes/index.js
@@ -59,4 +59,10 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+/*
+  API
+*/
+
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
